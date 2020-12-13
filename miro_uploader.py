eb4c317636ddd785c9d90181fc163462267bd76d
mateusz.milian <mateusz.milian@gmail.com>
2020-12-13 12:01:08 +0100

    initial commit

---
+++ miro_uploader.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+from watchdog.observers import Observer
+from watchdog.events import RegexMatchingEventHandler
+from shutil import copy2
+from os import remove
+
+
+def main():
+    if len(sys.argv) < 2:
+        print(f"At least one arg is required. Usage: miro_uploader <destination_path> [monitored_path]")
+        exit(1)
+
+    destination_path = sys.argv[1]
+    monitored_path = sys.argv[2] if len(sys.argv) > 2 else '.'
+    def on_created(event):
+        print(f"hey, {event.src_path} has been created!")
+
+    def on_modified(event):
+        print(f"hey buddy, {event.src_path} has been modified")
+        return_path = copy2(event.src_path, destination_path)
+        print(f"hey, that a copy path {return_path}")
+        remove(event.src_path)
+
+
+    event_handler = RegexMatchingEventHandler(regexes=[r"^[\x00-\x7F]*jpg$"], ignore_directories=True, case_sensitive=False)
+    event_handler.on_created = on_created
+    event_handler.on_modified = on_modified
+    observer = Observer()
+    observer.schedule(event_handler, monitored_path, recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+    finally:
+        observer.stop()
+        observer.join()
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,37 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='miro_uploader',  # Required
+    version='0.1',  # Required
+    description='A simple tool to upload/push new files to provided git repo',  # Optional
+    url='https://github.com/pypa/sampleproject',  # Optional
+    author='smate82',  # Optional
+    author_email='author@example.com',  # Optional
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+   ],
+    py_modules=["my_module"],
+    #
+    #packages=find_packages(where='src'),  # Required
+    python_requires='>=3.5, <4',
+    install_requires=['peppercorn'],  # Optional
+    entry_points={  # Optional
+        'console_scripts': [
+            'miro_uploader=miro_uploader:main',
+        ],
+    }
+)
